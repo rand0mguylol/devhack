4137367be79d3091939aacc21aa96bbe912ab55c
Yuan <waiiyuanchai21@gmail.com>
2023-11-08 01:36:00 +0800

    WrapperHalf component

---
+++ app/components/Common/WrapperHalf.js
@@ -0,0 +1,28 @@
+import { Container } from "@mui/material";
+
+export default function WrapperHalf({ props, children }) {
+
+  const isFull = props?.isFull ? '100%' : 'auto'
+  console.log(isFull)
+
+  return (
+    <Container
+      disableGutters
+      className=""
+      maxWidth="lg"
+      sx={{
+        margin: 0,
+        paddingBottom: "20px",
+        color: "white",
+        paddingTop: "15px",
+        // backgroundColor: "#F6F7FC",
+        paddingLeft: '22px',
+        paddingRight: '22px',
+        width: "100%",
+        overflowY: 'scroll',
+      }}
+    >
+      {children}
+    </Container>
+  );
+}
